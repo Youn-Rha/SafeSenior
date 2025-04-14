83c0687bc935cdfe34fe0ecf9bfff4c12874a635
heonniy <dkgus7535@naver.com>
2025-04-14 14:39:50 +0900

    feat: (Tracking step1) - Add Kalman filter for centroid prediction

--- src/dataCollect/addCluster.py
+++ src/dataCollect/addCluster.py
@@ -2,13 +2,42 @@ import pandas as pd
 from sklearn.cluster import DBSCAN
 from scipy.spatial import distance
 import numpy as np
-import os
+from filterpy.kalman import KalmanFilter
 
 SEQ_SIZE = 5
 EPS = 0.6
 MIN_SAMPLE = 20
 
+class ClusterKalman:
+    def __init__(self, initial_pos):
+        self.kf = KalmanFilter(dim_x=6, dim_z=3)
+        dt = 1.0
+        self.kf.F = np.array([[1,0,0,dt,0,0],
+                              [0,1,0,0,dt,0],
+                              [0,0,1,0,0,dt],
+                              [0,0,0,1,0,0],
+                              [0,0,0,0,1,0],
+                              [0,0,0,0,0,1]])
+        self.kf.H = np.array([[1,0,0,0,0,0],
+                              [0,1,0,0,0,0],
+                              [0,0,1,0,0,0]])
+        self.kf.R *= 0.1
+        self.kf.P *= 1000
+        self.kf.Q *= 0.01
+        x, y, z = initial_pos
+        self.kf.x[:3] = np.array([[x], [y], [z]])
+
+    def predict(self):
+        self.kf.predict()
+        return self.kf.x[:3].flatten()
+
+    def update(self, measurement):
+        self.kf.update(measurement)
+
 class Cluster():        
+    def __init__(self):
+        self.trackers = {}
+
     def addCluster(self, data):
         clustered_data = self.cluster_data(data)[1]
         clust_dict = clustered_data.to_dict(orient='records')
@@ -21,87 +50,86 @@ class Cluster():
             curNum = elem['frameNum']
             if prevNum != curNum:
                 seq_cnt += 1
-                if prevNum + 1 != curNum:   # 연속성 끊기면 시퀸스를 처리
-                    # glob clust
+                if prevNum + 1 != curNum:
                     df = pd.DataFrame(seq)
                     if seq_cnt > 1:
-                        df = self.track_clusters(df, glob_clust_mem) # 연속 시퀸스 존재 시 글로벌 클러스터
+                        df = self.track_clusters(df, glob_clust_mem)
                         glob_clust_mem = df['global_cluster'].max() + 1
                     global_clustered = pd.concat([global_clustered, df])
                     seq = []
                     seq_cnt = 0
                 prevNum = curNum
             seq.append(elem)
-        # add remainings
         df = pd.DataFrame(seq)
         if seq_cnt > 1:
-            df = self.track_clusters(df, glob_clust_mem) # 연속 시퀸스 존재 시 글로벌 클러스터
+            df = self.track_clusters(df, glob_clust_mem)
         global_clustered = pd.concat([global_clustered, df])
 
         global_clustered["global_cluster"] = global_clustered["global_cluster"].fillna(-1)
-
         return global_clustered
-    
+
     def cluster_data(self, data):
         seq_size = SEQ_SIZE
         unique_frame_numbers = {frame: idx + 1 for idx, frame in enumerate(data['frameNum'].unique())}
         data['frameNum'] = data['frameNum'].map(unique_frame_numbers)
 
         clustered_data = pd.DataFrame(columns=data.columns)
-
         for frame, group in data.groupby('frameNum'):
             group_data = group[['xPos', 'yPos', 'zPos']]
-
             dbscan = DBSCAN(eps=EPS, min_samples=MIN_SAMPLE)
             clusters = dbscan.fit_predict(group_data)
             group['cluster'] = clusters
-            clustered_data = pd.concat([clustered_data.dropna(axis=1, how='all'), group.dropna(axis=1, how='all')])
+            clustered_data = pd.concat([clustered_data, group])
 
         clustered_data = clustered_data[clustered_data['cluster'] != -1].reset_index().drop(columns='index')
         return list(set([i for i in range(1, seq_size + 1)]) - set(clustered_data['frameNum'].unique())), clustered_data
 
-    def kth_smallest_value(self, matrix, k):
-            flattened = np.ravel(matrix)
-            sorted_values = np.sort(flattened)
-            return sorted_values[k]
-
     def track_clusters(self, clustered_data, glob_clust_start):
-            clustered_data_agg = clustered_data.groupby(['frameNum', 'cluster']).agg({'frameNum': 'first','cluster': 'first','xPos': 'mean','yPos': 'mean','zPos': 'mean'}).reset_index(drop=True)
-            first_frame_num = clustered_data_agg['frameNum'].min()
-            global_clustered_data = clustered_data_agg[clustered_data_agg['frameNum'] == first_frame_num].copy()  # 제일 처음 프레임을 찾겠다
-            global_clustered_data.loc[:,'global_cluster'] = global_clustered_data['cluster'].copy()
+        clustered_data_agg = clustered_data.groupby(['frameNum', 'cluster']).agg(
+            {'frameNum': 'first','cluster': 'first','xPos': 'mean','yPos': 'mean','zPos': 'mean'}
+        ).reset_index(drop=True)
 
-            frame_len = len(clustered_data_agg['frameNum'].unique())
-            generated_cluster_n = int(global_clustered_data['global_cluster'].max()) + 1
+        first_frame_num = clustered_data_agg['frameNum'].min()
+        global_clustered_data = clustered_data_agg[clustered_data_agg['frameNum'] == first_frame_num].copy()
+        global_clustered_data.loc[:, 'global_cluster'] = global_clustered_data['cluster'].copy()
 
-            for i in range(first_frame_num, first_frame_num + frame_len - 1):
-                current_f = global_clustered_data[global_clustered_data['frameNum'] == i].reset_index().drop(columns='index')
-                next_f = clustered_data_agg[clustered_data_agg['frameNum'] == i + 1].reset_index().drop(columns='index')
-                used_current_f = [0 for i in range(len(current_f))]
-                distance_matrix = distance.cdist(current_f[['xPos', 'yPos', 'zPos']], next_f[['xPos', 'yPos', 'zPos']], metric='euclidean')
-                for j in range(distance_matrix.size):
-                    min_value = self.kth_smallest_value(distance_matrix, j)
-                    row, col = np.where(distance_matrix == min_value)
-                    row = row[0]
-                    col = col[0]
-                    if used_current_f[row] == 1:
-                        continue
-                    used_current_f[row] = 1
-                    if min_value >= 3.5:
-                        next_f.loc[col, 'global_cluster'] = None
-                    else:
-                        next_f.loc[col, 'global_cluster'] = current_f.loc[row, 'global_cluster']
-                g_arr = [i for i in range (generated_cluster_n, generated_cluster_n + next_f['global_cluster'].isna().sum())]
-                generated_cluster_n = generated_cluster_n + next_f['global_cluster'].isna().sum()
-                next_f.loc[next_f['global_cluster'].isna(), 'global_cluster'] = g_arr[:]
-                global_clustered_data = pd.concat([global_clustered_data, next_f]).reset_index().drop(columns='index')
-            # global cluster의 시작 번호를 적용
-            global_clustered_data["global_cluster"] = global_clustered_data["global_cluster"].fillna(-1 * glob_clust_start - 1) + glob_clust_start
+        for idx, row in global_clustered_data.iterrows():
+            g_id = row['global_cluster'] + glob_clust_start
+            self.trackers[g_id] = ClusterKalman(row[['xPos','yPos','zPos']].values)
 
-            # 원래 데이터와 합산
-            global_clustered_data = global_clustered_data.drop(columns=["xPos","yPos","zPos"])
-            # ret = pd.merge(clustered_data, global_clustered_data, on=["frameNum","cluster"], how="left")
-            ret = pd.merge(clustered_data, global_clustered_data[['cluster','frameNum','global_cluster']], on=['cluster','frameNum'], how='left')
-            return ret
+        frame_len = len(clustered_data_agg['frameNum'].unique())
+        generated_cluster_n = int(global_clustered_data['global_cluster'].max()) + 1
 
-# global_clustered.to_csv(dir)
+        for i in range(first_frame_num, first_frame_num + frame_len - 1):
+            current_f = global_clustered_data[global_clustered_data['frameNum'] == i].reset_index().drop(columns='index')
+            next_f = clustered_data_agg[clustered_data_agg['frameNum'] == i + 1].reset_index().drop(columns='index')
+            used_current_f = [0 for _ in range(len(current_f))]
+
+            for idx_next, next_row in next_f.iterrows():
+                obs = next_row[['xPos', 'yPos', 'zPos']].values
+                best_dist = float('inf')
+                best_id = None
+
+                for idx_cur, cur_row in current_f.iterrows():
+                    g_id = cur_row['global_cluster'] + glob_clust_start
+                    pred = self.trackers[g_id].predict()
+                    dist = np.linalg.norm(pred - obs)
+
+                    if dist < best_dist:
+                        best_dist = dist
+                        best_id = g_id
+
+                if best_dist < 3.5:
+                    next_f.loc[idx_next, 'global_cluster'] = best_id
+                    self.trackers[best_id].update(obs)
+                else:
+                    next_f.loc[idx_next, 'global_cluster'] = generated_cluster_n
+                    self.trackers[generated_cluster_n] = ClusterKalman(obs)
+                    generated_cluster_n += 1
+
+            global_clustered_data = pd.concat([global_clustered_data, next_f]).reset_index().drop(columns='index')
+
+        global_clustered_data["global_cluster"] = global_clustered_data["global_cluster"].fillna(-1 * glob_clust_start - 1) + glob_clust_start
+        global_clustered_data = global_clustered_data.drop(columns=["xPos","yPos","zPos"])
+        ret = pd.merge(clustered_data, global_clustered_data[['cluster','frameNum','global_cluster']], on=['cluster','frameNum'], how='left')
+        return ret
